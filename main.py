7734c78266f424390f31ec366fa8ff30ec6aebf8
ynzpog <158124276+ynzpog@users.noreply.github.com>
2026-02-24 12:20:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("a3df354b953bb334ac2c05d15a62f6ba")
+LAT = 14.848560
+LONG = 120.982040
+account_sid = os.environ.get("AC1e0cdc4e0e6725d66e2fbd2ab552b5fc")
+auth_token = os.environ.get("cb1bb7cb5e50b1397d6555e78be865f5")
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+weather_params = {
+    "lat": LAT,
+    "lon": LONG,
+    "appid": api_key,
+    "cnt" : 4,
+}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an ☔",
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+639399101416",
+    )
+    print(message.status)
+else:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="No rain🌞🌞",
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+639399101416",
+    )
+    print(message.status)
